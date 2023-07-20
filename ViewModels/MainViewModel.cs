97d0a97e63bf43e2b7039aa2b73a85cf9536b445
ZhegMaks <zhegmaks@gmail.com>
2023-07-20 22:47:33 +0300

    change project structure

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop="")
+        {
+            PropertyChanged?.Invoke(this, new )
+        }
+    }
+}
